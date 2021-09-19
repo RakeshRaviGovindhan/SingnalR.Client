052df2767c9af0aae0a96c1f495e24e7f9d367ab
RakeshRaviGovindhan <rakeshravig001@gmail.com>
2021-09-19 18:09:56 +0530

    UserName corrected

--- SignalR-VideoCall/SignalR-VideoCall/Model/MessageModel.cs
+++ SignalR-VideoCall/SignalR-VideoCall/Model/MessageModel.cs
@@ -4,7 +4,7 @@ namespace SignalRVideoCall.Model
     public class MessageModel
     {
         public long ID { get; set; }
-        public string UseName { get; set; }
+        public string UserName { get; set; }
         public string Message { get; set; }
         public bool IsOwnerMessage { get; set; }
     }
